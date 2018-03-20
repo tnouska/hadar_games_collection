afdf0ae151ff8906a78391b5aa6d72b0adf2aa47
Teagan Nouska <tnouska@live.com>
2018-03-20 16:06:56 -0500

    all working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -19,8 +19,15 @@ function appendToDom(gameCollection) {
     for (let game of gameCollection) {
         console.log('GAME:', game);
         let tr = $('<tr></tr>');
-        tr.append('<td>' + game.name + '</td>');
-        tr.append('<td>' + game.cost + '</td>');
+        if(game.isClearance) {
+            tr.append('<td class="clearance">' + game.name + '</td>');
+            tr.append('<td class="clearance">' + game.cost + '</td>');
+
+        } else {
+            tr.append('<td>' + game.name + '</td>');
+            tr.append('<td>' + game.cost + '</td>');
+
+        }
         $('#gameContent').append(tr);
         
     }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
-const PORT = 4001;
+//if on heroku, use the provided port else || use 4001
+const PORT = process.env.PORT || 4001;
 let bodyParser = require('body-parser');
 const gameCollection = [];//Contains objects {name: 'game name', cost: 0.99}
 //configuires bodyParser for jQuery
@@ -16,10 +17,23 @@ app.get('/game', (req, res) => {
 app.post('/game', (req, res) => {
     //console.log(req.body);
     let gameToAdd = req.body;
+    let gameName = gameToAdd.name;
+    let gameCost = parseFloat(gameToAdd.cost);
+    gameToAdd.name += '!!!';
+    gameToAdd.tax = gameCost * 0.07;
+    gameToAdd.isClearance = isClearance(gameCost);
     gameCollection.push(gameToAdd);
     console.log(gameCollection);
     res.sendStatus(200);
 })
+function isClearance(gameCost) {
+
+    if (gameCost - Math.floor(gameCost) === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 //spin up the server
 app.listen(PORT, () => {
